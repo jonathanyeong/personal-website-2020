ff6a8e922de137dc899ccc0c3b2598d82e8db4df
Jonathan Yeong <jonathanyeong@gmail.com>
2020-12-24 22:51:32 -0800

    Get Tailwind + Postcss working

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,6 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const striptags = require("striptags");
 
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
@@ -71,13 +70,10 @@ module.exports = function(eleventyConfig) {
     return collection.getFilteredByGlob('./src/posts/*.md')
       .filter(livePosts);
   });
-  eleventyConfig.addWatchTarget("src/assets/styles/");
 
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/icons");
-  eleventyConfig.addPassthroughCopy("src/assets/styles");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
-
   eleventyConfig.addPassthroughCopy("src/_redirects");
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
@@ -108,7 +104,6 @@ module.exports = function(eleventyConfig) {
     ghostMode: false
   });
 
-  eleventyConfig.setUseGitIgnore(false);
   return {
     templateFormats: [
       "md",
@@ -118,21 +113,10 @@ module.exports = function(eleventyConfig) {
       "11ty"
     ],
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
 
-    // These are all optional, defaults are shown:
     dir: {
       input: "src",
       includes: "_includes",
@@ -160,4 +144,4 @@ function extractExcerpt(article) {
     .concat("...");
 
   return excerpt;
-}
+};
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.njk',
+      './src/**/*.md',
+      './src/**/*.js'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
