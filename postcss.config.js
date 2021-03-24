22e70887b4417c49857817018bce5f55543b9c00
Jonathan Yeong <jonathanyeong@gmail.com>
2021-03-23 20:06:58 -0700

    Add cssnano and run it in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const cssnano = require('cssnano')({
+    preset: 'default',
+})
+
 module.exports = {
-  plugins: {
-    '@tailwindcss/jit': {},
-    autoprefixer: {},
-  }
+  plugins: [
+    require('@tailwindcss/jit'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [cssnano]
+      : []
+  ]
 }
